3ead87ecd0aa1f312f1d9a726323f10cd6286a42
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-07-01 14:18:45 -0500

    created constructors

--- src/main/java/com/example/demo/appuser/AppUser.java
+++ src/main/java/com/example/demo/appuser/AppUser.java
@@ -27,6 +27,17 @@ public class AppUser implements UserDetails {
     private Boolean locked;
     private Boolean enabled;
 
+    public AppUser(Long id, String name, String username, String email, String password, AppUserRole appUserRole, Boolean locked, Boolean enabled) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.appUserRole = appUserRole;
+        this.locked = locked;
+        this.enabled = enabled;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         SimpleGrantedAuthority authority = new SimpleGrantedAuthority(appUserRole.name());
