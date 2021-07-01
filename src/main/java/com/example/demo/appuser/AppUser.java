e0f1cd6379e137ae07fe42231ec200b4ea46cc4c
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-07-01 14:20:18 -0500

    entity and Id

--- src/main/java/com/example/demo/appuser/AppUser.java
+++ src/main/java/com/example/demo/appuser/AppUser.java
@@ -8,6 +8,8 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.Collection;
 import java.util.Collections;
 
@@ -15,9 +17,11 @@ import java.util.Collections;
 @Setter
 @EqualsAndHashCode
 @NoArgsConstructor
+@Entity
 
 public class AppUser implements UserDetails {
 
+    @Id
     private Long id;
     private String name;
     private String username;
