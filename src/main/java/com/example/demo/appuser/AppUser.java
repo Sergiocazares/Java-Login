9acde5720ae962e9a4c7c809c9214191515be610
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-06-30 21:43:27 -0500

    Lombok

--- src/main/java/com/example/demo/appuser/AppUser.java
+++ src/main/java/com/example/demo/appuser/AppUser.java
@@ -1,11 +1,20 @@
 package com.example.demo.appuser;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.Collections;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
 
 public class AppUser implements UserDetails {
 
