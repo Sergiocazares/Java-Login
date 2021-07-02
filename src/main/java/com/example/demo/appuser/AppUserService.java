d0ea9e47775305101ef13235b1286bed7939ad7a
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-07-02 13:37:55 -0500

    Student Repo

--- src/main/java/com/example/demo/appuser/AppUserService.java
+++ src/main/java/com/example/demo/appuser/AppUserService.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class AppUserService implements UserDetailsService {
+
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
         return null;
