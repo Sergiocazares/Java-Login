a872d995d413f1e80939006ad06610a9a2a67561
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-07-05 18:14:20 -0500

    optional appuser

--- src/main/java/com/example/demo/appuser/StudentRepository.java
+++ src/main/java/com/example/demo/appuser/StudentRepository.java
@@ -3,6 +3,10 @@ package com.example.demo.appuser;
 
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Transactional(readOnly = true)
 public interface StudentRepository {
+    Optional<AppUser> findByEmail(String email);
+
 }
