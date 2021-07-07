7aca0f95301477378e0dd29bc23ed282efca0a81
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-07-06 20:50:51 -0500

    All args constructor appuserService

--- src/main/java/com/example/demo/appuser/AppUserService.java
+++ src/main/java/com/example/demo/appuser/AppUserService.java
@@ -1,17 +1,18 @@
 package com.example.demo.appuser;
 
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class AppUserService implements UserDetailsService {
 
-    private final AppUser
+    private final AppUserRepository appUserRepository;
 
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
     }
 }
