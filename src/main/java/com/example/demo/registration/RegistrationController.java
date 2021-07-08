269be03aa3b515cadbb7cb39b950cabd10a9df94
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-07-08 14:01:25 -0500

    register

--- src/main/java/com/example/demo/registration/RegistrationController.java
+++ src/main/java/com/example/demo/registration/RegistrationController.java
@@ -1,6 +1,7 @@
 package com.example.demo.registration;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "api/v1/registration")
 @AllArgsConstructor
 public class RegistrationController {
+
+    public String register(@RequestBody RegistrationRequest request) {
+        return registrationService.register(request);
+    }
+
 }
