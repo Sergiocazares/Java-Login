2dcf5b40a19cad887249516d7f65359d9798186b
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-07-13 21:40:01 -0500

    config, constructor and web security

--- src/main/java/com/example/demo/security/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/security/config/WebSecurityConfig.java
@@ -1,4 +1,11 @@
 package com.example.demo.security.config;
 
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
 public class WebSecurityConfig {
 }
