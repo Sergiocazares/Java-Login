1c37b7c7a6e04212b80737292e2140fe38886482
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-07-16 21:31:48 -0500

    BCrypt password encoder

--- src/main/java/com/example/demo/PasswordEncoder.java
+++ src/main/java/com/example/demo/PasswordEncoder.java
@@ -1,7 +1,12 @@
 package com.example.demo;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class PasswordEncoder {
+
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
