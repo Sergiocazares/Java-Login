cd976565a44dba1ab132dc89b17ba7bd1abf61ab
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-07-20 16:34:46 -0500

    implements method string boolean

--- src/main/java/com/example/demo/registration/EmailValidator.java
+++ src/main/java/com/example/demo/registration/EmailValidator.java
@@ -3,5 +3,8 @@ package com.example.demo.registration;
 import java.util.function.Predicate;
 
 public class EmailValidator implements Predicate<String> {
-
+    @Override
+    public boolean test(String s) {
+        return false;
+    }
 }
