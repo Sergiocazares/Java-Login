fc6369c140f817d4bd9fc5219da4ab605ea066b2
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-07-21 17:15:23 -0500

    boolean valid email

--- src/main/java/com/example/demo/registration/RegistrationService.java
+++ src/main/java/com/example/demo/registration/RegistrationService.java
@@ -7,7 +7,14 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 public class RegistrationService {
+
+    private EmailValidator emailValidator;
+
     public String register(RegistrationRequest request){
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if (!isValidEmail) {
+            throw new IllegalArgumentException("Email not valid");
+        }
         return "works";
     }
 }
