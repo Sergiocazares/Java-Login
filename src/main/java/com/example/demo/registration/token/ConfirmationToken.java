434326f1060e691693b91aa383d8c0d5c9783025
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-07-28 14:52:05 -0500

    pirvate long string and local date time

--- src/main/java/com/example/demo/registration/token/ConfirmationToken.java
+++ src/main/java/com/example/demo/registration/token/ConfirmationToken.java
@@ -1,4 +1,12 @@
 package com.example.demo.registration.token;
 
+import java.time.LocalDateTime;
+
 public class ConfirmationToken {
+
+    private Long id;
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiredAt;
+    private LocalDateTime confirmAt;
 }
