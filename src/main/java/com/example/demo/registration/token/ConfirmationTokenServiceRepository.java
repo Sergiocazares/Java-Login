c691cbc16534ebe0dfb01a2b01bad66542e30052
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-08-05 14:04:51 -0500

    repo and extends jparepo with token and long

--- src/main/java/com/example/demo/registration/token/ConfirmationTokenServiceRepository.java
+++ src/main/java/com/example/demo/registration/token/ConfirmationTokenServiceRepository.java
@@ -1,4 +1,9 @@
 package com.example.demo.registration.token;
 
-public interface ConfirmationTokenServiceRepository {
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ConfirmationTokenServiceRepository extends JpaRepository<ConfirmationToken, Long> {
 }
