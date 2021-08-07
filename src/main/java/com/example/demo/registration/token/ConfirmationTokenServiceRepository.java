76c28f8371ac2086119e4723ad25126ed7a1d400
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-08-06 19:31:20 -0500

    optional confirmation token

--- src/main/java/com/example/demo/registration/token/ConfirmationTokenServiceRepository.java
+++ src/main/java/com/example/demo/registration/token/ConfirmationTokenServiceRepository.java
@@ -4,6 +4,11 @@ package com.example.demo.registration.token;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 @Repository
 public interface ConfirmationTokenServiceRepository extends JpaRepository<ConfirmationToken, Long> {
+
+    Optional<ConfirmationToken> findByToken(String token);
 }
