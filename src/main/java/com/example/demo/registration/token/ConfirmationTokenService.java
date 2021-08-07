190cda019f4f562170281e821b7d29e2c6ea0988
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-08-06 19:32:42 -0500

    args cons and service for confirmation token service

--- src/main/java/com/example/demo/registration/token/ConfirmationTokenService.java
+++ src/main/java/com/example/demo/registration/token/ConfirmationTokenService.java
@@ -1,4 +1,11 @@
 package com.example.demo.registration.token;
 
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class ConfirmationTokenService {
+
 }
